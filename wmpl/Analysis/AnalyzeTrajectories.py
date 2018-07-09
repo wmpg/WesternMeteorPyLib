fixed intersecting planes computation

wmpl/Analysis/AnalyzeTrajectories.py
@@ -136,6 +136,11 @@ def collectTrajPickles(dir_path, traj_type='original', unique=False):
     elif traj_type == 'milig':
         pickle_files = [pickle_f for pickle_f in pickle_files if '_milig.pickle' in pickle_f]
 
+
+    # Select intersecting planes trajectories
+    elif traj_type == 'planes':
+        pickle_files = [pickle_f for pickle_f in pickle_files if '_planes.pickle' in pickle_f]
+
     # Select gural trajectory
     elif 'gural' in traj_type:
         pickle_files = [pickle_f for pickle_f in pickle_files if '_{:s}_trajectory.pickle'.format(traj_type) \
@@ -500,26 +505,32 @@ if __name__ == "__main__":
     #dir_path = "../SimulatedMeteors/CABERNET/2011Draconids"
 
     #dir_path = "../SimulatedMeteors/CAMO/2011Draconids"
+    #dir_path = "../SimulatedMeteors/CAMO/2014Ursids"
     #dir_path = "../SimulatedMeteors/CAMO/2012Perseids"
 
-    #dir_path = "../SimulatedMeteors/SOMN_sim/2011Draconids"
-    dir_path = "../SimulatedMeteors/SOMN_sim/2014Ursids"
-    #dir_path = "../SimulatedMeteors/SOMN_sim/2012Perseids"
-    #dir_path = "../SimulatedMeteors/SOMN_sim/2015Taurids"
-    #dir_path = "../SimulatedMeteors/CAMSsim/2012Perseids"
     #dir_path = "../SimulatedMeteors/CAMSsim/2011Draconids"
     #dir_path = "../SimulatedMeteors/CAMSsim/2014Ursids"
+    #dir_path = "../SimulatedMeteors/CAMSsim/2012Perseids"
+
+    #dir_path = "../SimulatedMeteors/SOMN_sim/2011Draconids"
+    #dir_path = "../SimulatedMeteors/SOMN_sim/2014Ursids"
+    #dir_path = "../SimulatedMeteors/SOMN_sim/2012Perseids"
+    dir_path = "../SimulatedMeteors/SOMN_sim/2015Taurids"
 
 
     # Minimum convergence angle (deg)
     min_conv_angle = 1.0
 
+    # Radiant precision plot limits
+    sigma_r_max = 5.0 #deg
+    sigma_v_max = 5.0 #km/s
 
 
-    solvers = ['los', 'milig', 'mc', 'gural0', 'gural1', 'gural3']
-    plot_labels = ['LoS', 'MILIG', 'Monte Carlo', 'Gural (constant)', 'Gural (linear)', 'Gural (exp)']
-    markers = ['o', 's', '+', 'x', 'D', 'd']
-    sizes = [20, 20, 40, 40, 10, 20]
+
+    solvers = ['planes', 'los', 'milig', 'mc', 'gural0', 'gural1', 'gural3']
+    plot_labels = ['IP', 'LoS', 'MILIG', 'Monte Carlo', 'Gural (constant)', 'Gural (linear)', 'Gural (exp)']
+    markers = ['v', 'o', 's', '+', 'x', 'D', 'd']
+    sizes = [20, 20, 20, 40, 40, 10, 20]
 
 
 
@@ -528,72 +539,74 @@ if __name__ == "__main__":
 
 
 
-    # ### PLOT ORBIT ELEMENTS ###
-    # ##########################################################################################################
+    ### PLOT ORBIT ELEMENTS ###
+    ##########################################################################################################
 
-    # # Plot simulated meteors
-    # plt_handle = plotOrbitElements(sim_meteors, plt_type='sol_a', label='Simulated', marker='o', s=5)
-
-
-    # # Make sure all lengths are the same
-    # if sum([len(solvers), len(plot_labels), len(markers), len(sizes)]) != 4*len(solvers):
-    #     print('The lenghts of solvers, plots, markers and sizes is not the same!')
-    #     sys.exit()
-
-    # # Make plots from data with different solvers
-
-    # for solver, plt_lbl, marker, size in zip(solvers, plot_labels, markers, sizes):
-
-    #     # Load trajectories
-    #     traj_list = collectTrajPickles(dir_path, traj_type=solver)
+    # Plot simulated meteors
+    plt_handle = plotOrbitElements(sim_meteors, plt_type='sol_a', label='Simulated', marker='o', s=5)
 
 
-    #     # Filter by convergence angle
-    #     if 'gural' in solver:
+    # Make sure all lengths are the same
+    if sum([len(solvers), len(plot_labels), len(markers), len(sizes)]) != 4*len(solvers):
+        print('The lenghts of solvers, plots, markers and sizes is not the same!')
+        sys.exit()
 
-    #         # Remove all trajectories with the convergence angle less then 15 deg
-    #         traj_list = [traj for traj in traj_list if np.degrees(traj.max_convergence) >= min_conv_angle]
+    # Make plots from data with different solvers
 
-    #         pass
+    for solver, plt_lbl, marker, size in zip(solvers, plot_labels, markers, sizes):
 
-    #     else:
-
-    #         # Remove all trajectories with the convergence angle less then 15 deg
-    #         traj_list = [traj for traj in traj_list if np.degrees(traj.best_conv_inter.conv_angle) \
-    #             >= min_conv_angle]
+        # Load trajectories
+        traj_list = collectTrajPickles(dir_path, traj_type=solver)
 
 
-    #     # Plot trajectories
-    #     plt_handle = plotOrbitElements(traj_list, plt_type='sol_a', plt_handle=plt_handle, label=plt_lbl, \
-    #         alpha=0.5, s=size, marker=marker)
+        # Filter by convergence angle
+        if 'gural' in solver:
+
+            # Remove all trajectories with the convergence angle less then 15 deg
+            traj_list = [traj for traj in traj_list if np.degrees(traj.max_convergence) >= min_conv_angle]
+
+            pass
+
+        else:
+
+            # Remove all trajectories with the convergence angle less then 15 deg
+            traj_list = [traj for traj in traj_list if np.degrees(traj.best_conv_inter.conv_angle) \
+                >= min_conv_angle]
+
+
+        # Plot trajectories
+        plt_handle = plotOrbitElements(traj_list, plt_type='sol_a', plt_handle=plt_handle, label=plt_lbl, \
+            alpha=0.5, s=size, marker=marker)
 
 
     
-    # # Get the limits of the plot
-    # x_min, x_max = plt_handle.get_xlim()
+    # Get the limits of the plot
+    x_min, x_max = plt_handle.get_xlim()
 
     
-    # # Plot the 7:2 resonance with Jupiter
-    # sol_arr = np.linspace(x_min, x_max, 10)
+    # Plot the 7:2 resonance with Jupiter
+    sol_arr = np.linspace(x_min, x_max, 10)
 
-    # plt.plot(sol_arr, np.zeros_like(sol_arr) + 2.24, linestyle='--', color='k')
-    # plt.plot(sol_arr, np.zeros_like(sol_arr) + 2.28, linestyle='--', color='k')
+    plt.plot(sol_arr, np.zeros_like(sol_arr) + 2.24, linestyle='--', color='k')
+    plt.plot(sol_arr, np.zeros_like(sol_arr) + 2.28, linestyle='--', color='k')
 
-    # # Set xlim
-    # plt.xlim([x_min, x_max])
+    # Set xlim
+    plt.xlim([x_min, x_max])
 
-    # # Limit a from 1.5 AU to 3.5 AU
-    # plt.ylim([1.5, 3.5])
+    # Limit a from 1.5 AU to 3.5 AU
+    plt.ylim([1.5, 3.5])
 
 
-    # plt.legend()
+    plt.legend()
 
-    # # Save the figure
-    # plt.savefig(os.path.join(dir_path, 'solver_comparison_sol_a.png'), dpi=300)
+    # Save the figure
+    plt.savefig(os.path.join(dir_path, 'solver_comparison_sol_a.png'), dpi=300)
 
-    # plt.show()
+    plt.show()
 
-    # ##########################################################################################################
+    sys.exit()
+
+    ##########################################################################################################
 
 
 
@@ -628,7 +641,7 @@ if __name__ == "__main__":
 
 
         # Plot the 2D histogram comparing the results, radiants within X degrees, Vg within X km/s
-        compareTrajToSim(dir_path, sim_meteors, traj_list, solver_name, 5.0, 5.0, vmax=10)
+        compareTrajToSim(dir_path, sim_meteors, traj_list, solver_name, sigma_r_max, sigma_v_max, vmax=10)
 
 
 

wmpl/Formats/CAMS.py
@@ -617,6 +617,7 @@ def computeAbsoluteMagnitudes(traj, meteor_list):
 
 if __name__ == "__main__":
 
+    import matplotlib
     import matplotlib.pyplot as plt
 
     #dir_path = "../DenisGEMcases"
@@ -624,13 +625,16 @@ if __name__ == "__main__":
     #dir_path = "/home/dvida/DATA/Dropbox/Apps/VSA2017 RMS data/first_rpi_orbit"
     #dir_path = "D:/Dropbox/Apps/VSA2017 RMS data/first_rpi_orbit"
     #dir_path = "/home/dvida/DATA/Dropbox/Apps/VSA2017 RMS data/first_rpi_orbit"
-    dir_path = "D:/Dropbox/RPi Meteor Station/orbit_test"
+    #dir_path = "D:/Dropbox/RPi Meteor Station/orbit_test"
     #dir_path = "/home/dvida/Dropbox/RPi Meteor Station/orbit_test"
-    #dir_path = "/home/dvida/Dropbox/RPi Meteor Station/orbit_test/20180615_long_shallow_meteor"
+    dir_path = "/home/dvida/Dropbox/RPi Meteor Station/orbit_test/20180615_long_shallow_meteor"
     #dir_path = "D:/Dropbox/RPi Meteor Station/orbit_test/20180615_long_shallow_meteor"
 
     # Find the absolute path of the given directory
     dir_path = os.path.abspath(dir_path)
+
+    # Image file type of the plots
+    plot_file_type = 'pdf'
 
     camerasites_file_name = 'CameraSites.txt'
     cameratimeoffsets_file_name = 'CameraTimeOffsets.txt'
@@ -638,9 +642,9 @@ if __name__ == "__main__":
     #ftpdetectinfo_file_name = 'FTPdetectinfo_first_rpi.txt'
     #ftpdetectinfo_file_name = 'FTPdetectinfo_20180614.txt'
     #ftpdetectinfo_file_name = "FTPdetectinfo_20180614_temporal.txt"
-    ftpdetectinfo_file_name = "FTPdetectinfo_20180614_spatial.txt"
+    #ftpdetectinfo_file_name = "FTPdetectinfo_20180614_spatial.txt"
     #ftpdetectinfo_file_name = 'FTPdetectinfo_20180615.txt'
-    #ftpdetectinfo_file_name = 'FTPdetectinfo_20180615_long_shallow_meteor.txt'
+    ftpdetectinfo_file_name = 'FTPdetectinfo_20180615_long_shallow_meteor.txt'
 
 
     camerasites_file_name = os.path.join(dir_path, camerasites_file_name)
@@ -678,58 +682,62 @@ if __name__ == "__main__":
     #   print('--------------------------')
     #   print(met)
 
-    meteor = meteor6
+    meteor = meteor1
 
     # Run the trajectory solver
-    traj = solveTrajectoryCAMS(meteor, os.path.join(dir_path, 'meteor6_spatial'), solver='original', monte_carlo=False, \
-        show_plots=False, save_results=True, mc_noise_std=1.0, mc_runs=250, max_toffset=60.0)
+    traj = solveTrajectoryCAMS(meteor, os.path.join(dir_path, 'trajectory'), solver='original', monte_carlo=False, \
+        show_plots=False, save_results=True, mc_noise_std=1.0, mc_runs=250, max_toffset=60.0, 
+        plot_file_type=plot_file_type)
 
 
-    # ### PERFORM PHOTOMETRY
+    ### PERFORM PHOTOMETRY
 
-    # # Compute absolute mangitudes
-    # computeAbsoluteMagnitudes(traj, meteor)
+    # Override default DPI for saving from the interactive window
+    matplotlib.rcParams['savefig.dpi'] = 300
 
-    # # List of photometric uncertainties per station
-    # photometry_stddevs = [0.3]*len(meteor)
-    # # photometry_stddevs = [0.2, 0.3]*len(meteor)
+    # Compute absolute mangitudes
+    computeAbsoluteMagnitudes(traj, meteor)
 
-    # time_data_all = []
-    # abs_mag_data_all = []
+    # List of photometric uncertainties per station
+    photometry_stddevs = [0.3]*len(meteor)
+    # photometry_stddevs = [0.2, 0.3]*len(meteor)
 
-    # # Plot absolute magnitudes for every station
-    # for i, (meteor_obs, photometry_stddev) in enumerate(zip(meteor, photometry_stddevs)):
+    time_data_all = []
+    abs_mag_data_all = []
 
-    #     # Take only magnitudes that are not None
-    #     good_mag_indices = [j for j, abs_mag in enumerate(meteor_obs.abs_mag_data) if abs_mag is not None]
-    #     time_data = traj.observations[i].time_data[good_mag_indices]
-    #     abs_mag_data = np.array(meteor_obs.abs_mag_data)[good_mag_indices]
+    # Plot absolute magnitudes for every station
+    for i, (meteor_obs, photometry_stddev) in enumerate(zip(meteor, photometry_stddevs)):
 
-    #     time_data_all += time_data.tolist()
-    #     abs_mag_data_all += abs_mag_data.tolist()
+        # Take only magnitudes that are not None
+        good_mag_indices = [j for j, abs_mag in enumerate(meteor_obs.abs_mag_data) if abs_mag is not None]
+        time_data = traj.observations[i].time_data[good_mag_indices]
+        abs_mag_data = np.array(meteor_obs.abs_mag_data)[good_mag_indices]
 
-    #     # Sort by time
-    #     temp_arr = np.c_[time_data, abs_mag_data]
-    #     temp_arr = temp_arr[np.argsort(temp_arr[:, 0])]
-    #     time_data, abs_mag_data = temp_arr.T
+        time_data_all += time_data.tolist()
+        abs_mag_data_all += abs_mag_data.tolist()
 
-    #     # Plot the magnitude
-    #     plt_hande = plt.plot(time_data, abs_mag_data, label=meteor_obs.station_id, zorder=3)
+        # Sort by time
+        temp_arr = np.c_[time_data, abs_mag_data]
+        temp_arr = temp_arr[np.argsort(temp_arr[:, 0])]
+        time_data, abs_mag_data = temp_arr.T
 
-    #     # Plot magnitude errorbars
-    #     plt.errorbar(time_data, abs_mag_data, yerr=photometry_stddev, fmt='--o', color=plt_hande[0].get_color())
+        # Plot the magnitude
+        plt_hande = plt.plot(time_data, abs_mag_data, label=meteor_obs.station_id, zorder=3)
+
+        # Plot magnitude errorbars
+        plt.errorbar(time_data, abs_mag_data, yerr=photometry_stddev, fmt='--o', color=plt_hande[0].get_color())
 
     
-    # plt.legend()
+    plt.legend()
 
-    # plt.xlabel('Time (s)')
-    # plt.ylabel('Absolute magnitude')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Absolute magnitude')
     
-    # plt.gca().invert_yaxis()
+    plt.gca().invert_yaxis()
 
-    # plt.grid()
+    plt.grid()
 
-    # plt.show()
+    plt.show()
 
 
     # ### Compute the mass

wmpl/TrajSim/ShowerSim.py
@@ -319,10 +319,23 @@ class SimMeteor(object):
         savePickle(self, output_dir, file_name_pickle)
 
 
+    def initOutput(self, output_dir):
+        """ Prepare everything for saving solver results. """
+
+        mkdirP(output_dir)
+
+        # File name of the report file
+        file_name = str(self.jdt_ref) + "_sim_met_info.txt"
+
+        self.file_path = os.path.join(output_dir, file_name)
+
 
 
     def saveInfo(self, output_dir):
         """ Save information about the simulated meteor. """
+
+
+        self.initOutput(self, output_dir)
 
         out_str =  ""
         out_str += "reference JD: {:20.12f}".format(self.jdt_ref) + "\n"
@@ -418,13 +431,6 @@ class SimMeteor(object):
 
         print(out_str)
 
-        mkdirP(output_dir)
-
-        # File name of the report file
-        file_name = str(self.jdt_ref) + "_sim_met_info.txt"
-
-        self.file_path = os.path.join(output_dir, file_name)
-
         # Save the report to a file
         with open(self.file_path, 'w') as f:
             f.write(out_str)
@@ -454,7 +460,7 @@ class SimMeteor(object):
         """
 
         # Add the info to the simulation file, if it exists
-        if self.file_path:
+        if os.path.isfile(self.file_path):
 
             out_str = '\n'
             out_str += '-----------------------------\n'
@@ -479,6 +485,8 @@ class SimMeteor(object):
 
 
             out_str += '\n'
+
+            print(out_str)
 
 
             with open(self.file_path, 'a') as f:
@@ -2984,110 +2992,4 @@ if __name__ == "__main__":
         sim_met.saveInfo(shower_dir)
 
         print('Saved meteor:', sim_met.jdt_ref)
-
-    
-
-
-    # Break here without running solvers!
-    sys.exit()
-
-
-
-
-
-    ##########################################################################################################
-    
-    # Trajectory solver (original or gural)
-    traj_solvers = ['planes', 'los', 'monte_carlo', 'gural']
-
-    # Solve generated trajectories
-    for met_no, sim_met in enumerate(sim_meteor_list):
-
-
-        # Directory where trajectory results will be saved
-        output_dir = os.path.join(shower_dir, "{:03d} - {:.6f}".format(met_no, sim_met.jdt_ref))
-
-
-        # Save info about the simulated meteor
-        sim_met.saveInfo(output_dir)
-
-
-        # Solve the simulated meteor with multiple solvers
-        for traj_solver in traj_solvers:
-
-
-            if (traj_solver == 'los') or (traj_solver == 'planes'):
-
-                # Init the trajectory (LoS or intersecing planes)
-                traj = Trajectory(sim_met.jdt_ref, output_dir=output_dir, max_toffset=t_max_offset, \
-                    meastype=2, show_plots=False, save_results=False, monte_carlo=False)
-
-
-            elif traj_solver == 'monte_carlo':
-                
-                # Init the trajectory
-                traj = Trajectory(sim_met.jdt_ref, output_dir=output_dir, max_toffset=t_max_offset, \
-                    meastype=2, show_plots=False, mc_runs=250)
-
-            elif traj_solver == 'gural':
-                
-                # Velocity model ID
-                velmodel = 3
-
-                # Init the new Gural trajectory solver object
-                traj = GuralTrajectory(len(sim_met.observations), sim_met.jdt_ref, output_dir=output_dir, 
-                    max_toffset=t_max_offset, meastype=2, velmodel=velmodel, verbose=1, show_plots=False)
-
-            else:
-                print(traj_solver, '- unknown trajectory solver!')
-                sys.exit()
-
-
-            # Fill in observations
-            for obs in sim_met.observations:
-
-                traj.infillTrajectory(obs.meas1, obs.meas2, obs.time_data, obs.lat, obs.lon, obs.ele, \
-                    station_id=obs.station_id)
-
-
-            # Solve trajectory
-            traj = traj.run()
-
-
-            # Calculate orbit of an intersecting planes solution
-            if traj_solver == 'planes':
-
-                # Use the average velocity of the first part of the trajectory for the initial velocity
-                time_vel = []
-                for obs in traj.observations:
-                    for t, v in zip(obs.time_data, obs.velocities):
-                        time_vel.append([t, v])
-
-                time_vel = np.array(time_vel)
-
-                # Sort by time
-                time_vel = time_vel[time_vel[:, 0].argsort()]
-
-                # Calculate the velocity of the first half of the trajectory
-                v_init_fh = np.mean(time_vel[int(len(time_vel)/2), 1])
-
-                # Calculate the orbit
-                traj.orbit = calcOrbit(traj.avg_radiant, v_init_fh, traj.v_avg, traj.state_vect_avg, \
-                traj.jd_avg, stations_fixed=True, reference_init=False)
-
-
-            if traj_solver == 'gural':
-
-                # Save info about the simulation comparison
-                sim_met.saveTrajectoryComparison(traj, traj_solver, 'velmodel: ' + str(velmodel))
-
-            else:
-                # Save info about the simulation comparison
-                sim_met.saveTrajectoryComparison(traj, traj_solver)
-
-
-            # Dump measurements to a MATLAB-style file
-            if traj_solver == 'monte_carlo':
-                traj.dumpMeasurements(output_dir, str(sim_met.jdt_ref) + '_meas_dump.txt')
-
 

wmpl/TrajSim/SimMeteorBatchSolve.py
@@ -29,18 +29,19 @@ if __name__ == "__main__":
 
     #shower_dir = os.path.abspath("../SimulatedMeteors/CABERNET/2011Draconids")
 
-    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMO/2011Draconids_TEST")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMO/2011Draconids")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMO/2014Ursids")
     #shower_dir = os.path.abspath("../SimulatedMeteors/CAMO/2012Perseids")
 
-    #shower_dir = os.path.abspath("../SimulatedMeteors/Perfect_CAMO/2011Draconids")
-    #shower_dir = os.path.abspath("../SimulatedMeteors/Perfect_CAMO/2011Draconids_TEST")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2011Draconids")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2014Ursids")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2012Perseids")
 
     #shower_dir = os.path.abspath("../SimulatedMeteors/SOMN_sim/2011Draconids")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/SOMN_sim/2014Ursids")
+    #shower_dir = os.path.abspath("../SimulatedMeteors/SOMN_sim/2012Perseids")
+    shower_dir = os.path.abspath("../SimulatedMeteors/SOMN_sim/2015Taurids")
     #shower_dir = os.path.abspath("../SimulatedMeteors/SOMN_precise_sim/LongFireball_grav")
-
-    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2011Draconids")
-    shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2014Ursids")
-    #shower_dir = os.path.abspath("../SimulatedMeteors/CAMSsim/2012Perseids")
 
     # Maximum time offset (seconds)
     t_max_offset = 1
@@ -50,9 +51,10 @@ if __name__ == "__main__":
 
 
     # Trajectory solvers
-    traj_solvers = ['planes', 'los', 'milig', 'monte_carlo', 'gural0', 'gural1', 'gural2', 'gural3']
+    #traj_solvers = ['planes', 'los', 'milig', 'monte_carlo', 'gural0', 'gural1', 'gural2', 'gural3']
     #traj_solvers = ['planes', 'los', 'monte_carlo']
     #traj_solvers = ['milig']
+    traj_solvers = ['planes', 'milig']
 
 
     ##########################################################################################################
@@ -79,8 +81,11 @@ if __name__ == "__main__":
         # Directory where trajectory results will be saved
         output_dir = os.path.join(shower_dir, "{:03d} - {:.6f}".format(met_no, sim_met.jdt_ref))
 
-        # Save info about the simulated meteor
-        sim_met.saveInfo(output_dir)
+        # Prepare everything for saving data to disk
+        sim_met.initOutput(output_dir)
+
+        # Save info about the simulated meteor !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # sim_met.saveInfo(output_dir)
 
         # Solve the simulated meteor with multiple solvers
         for traj_solver in traj_solvers:
@@ -139,7 +144,8 @@ if __name__ == "__main__":
                 # Use the average velocity of the first part of the trajectory for the initial velocity
                 time_vel = []
                 for obs in traj.observations:
-                    for t, v in zip(obs.time_data, obs.velocities):
+                    for t, v in zip(obs.time_data[1:], obs.velocities[1:]):
+
                         time_vel.append([t, v])
 
                 time_vel = np.array(time_vel)
@@ -148,7 +154,7 @@ if __name__ == "__main__":
                 time_vel = time_vel[time_vel[:, 0].argsort()]
 
                 # Calculate the velocity of the first half of the trajectory
-                v_init_fh = np.mean(time_vel[int(len(time_vel)/2), 1])
+                v_init_fh = np.mean(time_vel[:int(len(time_vel)/2), 1])
 
                 # Calculate the orbit
                 traj.orbit = calcOrbit(traj.avg_radiant, v_init_fh, traj.v_avg, traj.state_vect_avg, \
@@ -162,9 +168,24 @@ if __name__ == "__main__":
             # Calculate the orbit as MILIG does it, with the average velocity
             elif traj_solver == 'milig':
 
+                # Use the average velocity of the first half of the trajectory for the initial velocity
+                time_vel = []
+                for obs in traj.observations:
+                    for t, v in zip(obs.time_data[1:], obs.velocities[1:]):
+
+                        time_vel.append([t, v])
+
+                time_vel = np.array(time_vel)
+
+                # Sort by time
+                time_vel = time_vel[time_vel[:, 0].argsort()]
+
+                # Calculate the velocity of the first half of the trajectory
+                v_init_fh = np.mean(time_vel[:int(len(time_vel)/2), 1])
+
                 # Calculate the orbit with the average velocity
-                traj.orbit = calcOrbit(traj.radiant_eci_mini, traj.v_avg, traj.v_avg, traj.state_vect_mini, \
-                    traj.rbeg_jd, stations_fixed=False, reference_init=False)
+                traj.orbit = calcOrbit(traj.radiant_eci_mini, v_init_fh, traj.v_avg, traj.state_vect_mini, \
+                    traj.rbeg_jd, stations_fixed=False, reference_init=True)
 
                 # Save the simulated milig solution
                 savePickle(traj, output_dir, traj.file_name + '_milig.pickle')