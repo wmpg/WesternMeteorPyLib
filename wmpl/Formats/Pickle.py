7d015a03f7e71675c73e1e5829fa19f8d3228b66
denis.vida <denis.vida@gmail.com>
2019-09-16 16:25:33 -0400

    added interface to solving pickle files

--- wmpl/Formats/Pickle.py
+++ wmpl/Formats/Pickle.py
@@ -3,6 +3,7 @@
 from __future__ import print_function, division, absolute_import
 
 import os
+import sys
 
 import numpy as np
 
@@ -43,7 +44,7 @@ def dumpAsEvFiles(dir_path, file_name):
 
 
 
-def solveTrajectory(dir_path, file_name, solver='original', **kwargs):
+def solveTrajectoryPickle(dir_path, file_name, solver='original', **kwargs):
     """ Rerun the trajectory solver on the given trajectory pickle file. """
 
 
@@ -53,8 +54,20 @@ def solveTrajectory(dir_path, file_name, solver='original', **kwargs):
     # Run the PyLIG trajectory solver
     if solver == 'original':
 
+        # Given the max time offset from the pickle file and input, use the larger one of the two
+        max_toffset = traj_p.max_toffset
+        if "max_toffset" in kwargs:
+
+            if (kwargs["max_toffset"] is not None) and (traj_p.max_toffset is not None):
+
+                max_toffset = max(traj_p.max_toffset, kwargs["max_toffset"])
+
+            # Remove the max time offset from the list of keyword arguments
+            kwargs.pop("max_toffset", None)
+
+
         # Reinitialize the trajectory solver
-        traj = Trajectory(traj_p.jdt_ref, output_dir=dir_path, max_toffset=traj_p.max_toffset, \
+        traj = Trajectory(traj_p.jdt_ref, output_dir=dir_path, max_toffset=max_toffset, \
             meastype=traj_p.meastype, **kwargs)
 
 
@@ -104,25 +117,96 @@ def solveTrajectory(dir_path, file_name, solver='original', **kwargs):
 
 if __name__ == "__main__":
 
-    # dir_path = os.path.abspath("../SimulatedMeteors/CAMO_OLD/Perseids/2456149.60802")
-    # file_name = "20120810_023532_trajectory.pickle"
 
-    # dir_path = os.path.abspath("../SimulatedMeteors/CAMO_OLD/Perseids/2456150.7563")
-    # file_name = "20120811_060904_trajectory.pickle"
-
-    # dir_path = os.path.abspath("../SimulatedMeteors/CAMO/2011Draconids/023 - 2455843.361897")
-    # file_name = "20111008_204107_trajectory.pickle"
-
-    dir_path = os.path.abspath("/mnt/bulk/trajectories/20190606_002926.876_HR")
-    file_name = "20190606_002926_trajectory.pickle"
-
-    #dir_path = os.path.abspath("/home/dvida/Desktop/test/012 - 2455896.500000")
-    #file_name = "20111201_000000_trajectory.pickle"
+    import argparse
 
 
-    # # Dump the pickled trajectory as UWO-style ev_* file 
-    # dumpAsEvFiles(dir_path, file_name)
+    ### COMMAND LINE ARGUMENTS
 
+    # Init the command line arguments parser
+    arg_parser = argparse.ArgumentParser(description=""" Re-run the Monte Carlo trajectory solver on a trajectory pickle file.""",
+        formatter_class=argparse.RawTextHelpFormatter)
 
-    # Solve the trajectory from the given pickle file
-    traj = solveTrajectory(dir_path, file_name, solver='original', monte_carlo=False)
+    arg_parser.add_argument('input_file', type=str, help='Path to the .pickle file.')
+
+    arg_parser.add_argument('-s', '--solver', metavar='SOLVER', help="""Trajectory solver to use. \n
+        - 'original' - Monte Carlo solver
+        - 'gural0' - Gural constant velocity
+        - 'gural1' - Gural linear deceleration
+        - 'gural2' - Gural quadratic deceleration
+        - 'gural3' - Gural exponential deceleration
+         """, type=str, nargs='?', default='original')
+
+    arg_parser.add_argument('-t', '--maxtoffset', metavar='MAX_TOFFSET', nargs=1, \
+        help='Maximum time offset between the stations.', type=float)
+
+    arg_parser.add_argument('-v', '--vinitht', metavar='V_INIT_HT', nargs=1, \
+        help='The initial veloicty will be estimated as the average velocity above this height (in km). If not given, the initial velocity will be estimated using the sliding fit which can be controlled with the --velpart option.', \
+        type=float)
+
+    arg_parser.add_argument('-p', '--velpart', metavar='VELOCITY_PART', \
+        help='Fixed part from the beginning of the meteor on which the initial velocity estimation using the sliding fit will start. Default is 0.25 (25 percent), but for noisier data this might be bumped up to 0.5.', \
+        type=float, default=0.25)
+
+    arg_parser.add_argument('-d', '--disablemc', \
+        help='Do not use the Monte Carlo solver, but only run the geometric solution.', action="store_true")
+    
+    arg_parser.add_argument('-r', '--mcruns', metavar="MC_RUNS", nargs='?', \
+        help='Number of Monte Carlo runs.', type=int, default=100)
+
+    arg_parser.add_argument('-u', '--uncertgeom', \
+        help='Compute purely geometric uncertainties.', action="store_true")
+    
+    arg_parser.add_argument('-g', '--disablegravity', \
+        help='Disable gravity compensation.', action="store_true")
+
+    arg_parser.add_argument('-l', '--plotallspatial', \
+        help='Plot a collection of plots showing the residuals vs. time, length and height.', \
+        action="store_true")
+
+    arg_parser.add_argument('-i', '--imgformat', metavar='IMG_FORMAT', nargs=1, \
+        help="Plot image format. 'png' by default, can be 'pdf', 'eps',... ", type=str, default='png')
+
+    arg_parser.add_argument('-x', '--hideplots', \
+        help="Don't show generated plots on the screen, just save them to disk.", action="store_true")
+
+    # Parse the command line arguments
+    cml_args = arg_parser.parse_args()
+
+    ############################
+    
+
+    ### Parse command line arguments ###
+
+    max_toffset = None
+    print(cml_args.maxtoffset)
+    if cml_args.maxtoffset:
+        max_toffset = cml_args.maxtoffset[0]
+
+    velpart = None
+    if cml_args.velpart:
+        velpart = cml_args.velpart
+
+    vinitht = None
+    if cml_args.vinitht:
+        vinitht = cml_args.vinitht[0]
+
+    ### ###
+
+        
+    # Split the input directory and the file
+    if os.path.isfile(cml_args.input_file):
+
+        dir_path, file_name = os.path.split(cml_args.input_file)
+
+    else:
+        print('Input file: {:s}'.format(cml_args.input_file))
+        print('The given input file does not exits!')
+        sys.exit()
+
+    # Run the solver
+    solveTrajectoryPickle(dir_path, file_name, solver=cml_args.solver, max_toffset=max_toffset, \
+        monte_carlo=(not cml_args.disablemc), mc_runs=cml_args.mcruns, geometric_uncert=cml_args.uncertgeom, \
+        gravity_correction=(not cml_args.disablegravity), plot_all_spatial_residuals=cml_args.plotallspatial,
+        plot_file_type=cml_args.imgformat, show_plots=(not cml_args.hideplots), v_init_part=velpart, \
+        v_init_ht=vinitht)
