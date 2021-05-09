774cb442b48c7009f15ef6138ddfb56d5562da59
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-05-09 21:58:37 +0100

    Added optional param to plot shower loci on chart of whole time period

--- wmpl/Trajectory/AggregateAndPlot.py
+++ wmpl/Trajectory/AggregateAndPlot.py
@@ -1891,6 +1891,9 @@ if __name__ == "__main__":
         help="""During the first continous run, go back FIRST_PREV_SOLS to generate the plots and report. After that, use PREV_SOLS to run."""
         )
 
+    arg_parser.add_argument('-p', '--plot_all_showers', metavar='PLOTALLSHOWERS', type=int, \
+        help="""Plot showers on the maps showing the whole date ramge."""
+        )
     # Parse the command line arguments
     cml_args = arg_parser.parse_args()
 
@@ -1993,7 +1996,11 @@ if __name__ == "__main__":
 
         # Generate summary plots
         print("Plotting all trajectories...")
-        generateTrajectoryPlots(cml_args.dir_path, traj_list, plot_showers=False, time_limited_plot=False)
+        pas = False
+        if cml_args.plot_all_showers is not None:
+            if cml_args.plot_all_showers == 1:
+                pas = True
+        generateTrajectoryPlots(cml_args.dir_path, traj_list, plot_showers=pas, time_limited_plot=False)
 
         # Generate station plot
         print("Plotting station plot...")
