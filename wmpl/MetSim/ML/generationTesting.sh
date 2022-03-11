1dd845a17aa9fc54f2e182c2bd2c1c0fad447f0a
GoodStuff11 <uikluspe@gmail.com>
2022-03-10 20:20:24 -0500

    fixed bug

--- wmpl/MetSim/ML/generationTesting.sh
+++ wmpl/MetSim/ML/generationTesting.sh
@@ -1,5 +1,3 @@
-@echo off
-conda activate wmpl
 cd /home/jkambul2/WesternMeteorPyLib
 #start python -m wmpl.MetSim.ML.GenerateSimulations /home/jkambul2/files/fixed_erosion_dataset2 6000000 --noerosion --fixed 0 0 0 0 0 1 1 1 1 1
 python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixed_erosion_dataset2 /home/jkambul2/files/trained_models -mn fixederosion --grouping 256 50
@@ -12,4 +10,3 @@ python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixed_erosion_dataset2 
 
 python -m wmpl.MetSim.ML.GenerateSimulations /home/jkambul2/files/fixed_erosion_dataset2 6000000 --noerosion --fixed 0 0 0 0 1 0 1 1 1 1
 python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixed_erosion_dataset2 /home/jkambul2/files/trained_models -mn fixederosion --grouping 256 50
-pause
