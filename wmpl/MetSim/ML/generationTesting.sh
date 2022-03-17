5004c4eca462678bb5861aecbc0d4d2f6e2c6df9
GoodStuff11 <uikluspe@gmail.com>
2022-03-17 00:51:37 -0400

    chagne

--- wmpl/MetSim/ML/generationTesting.sh
+++ wmpl/MetSim/ML/generationTesting.sh
@@ -14,7 +14,7 @@ cd /home/jkambul2/WesternMeteorPyLib
 # python -m wmpl.MetSim.ML.GenerateSimulations /home/jkambul2/files/log_sigma_rho 600000 --fixed 0 0 0 0 0 1 1 1 1 1 --noerosion
 # python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/log_sigma_rho /home/jkambul2/files/trained_models -mn log_sigma_rho --grouping 256 50
 
-python -m wmpl.MetSim.ML.GenerateSimulations /home/jkambul2/files/fixederosion_restrictedrange_dataset 6000000 --noerosion --fixed 0 0 0 0 0 1 1 1 1 1
+# python -m wmpl.MetSim.ML.GenerateSimulations /home/jkambul2/files/fixederosion_restrictedrange_dataset 600000 --noerosion --fixed 0 0 0 0 0 1 1 1 1 1
 python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixederosion_restrictedrange_dataset /home/jkambul2/files/trained_models -mn fixederosion_reduceddomain --grouping 256 50 --roi -1
 python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixederosion_restrictedrange_dataset /home/jkambul2/files/trained_models -mn lowerleft_fixederosion --grouping 256 50 --roi 0
 python -m wmpl.MetSim.ML.FitErosion /home/jkambul2/files/fixederosion_restrictedrange_dataset /home/jkambul2/files/trained_models -mn lowerright_fixederosion --grouping 256 50 --roi 1
