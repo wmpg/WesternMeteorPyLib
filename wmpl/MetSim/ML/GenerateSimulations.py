87836be63fbe5136f11ffd58a82b697bd892f2b1
GoodStuff11 <uikluspe@gmail.com>
2022-03-28 14:24:47 -0400

    fixed bug

--- wmpl/MetSim/ML/GenerateSimulations.py
+++ wmpl/MetSim/ML/GenerateSimulations.py
@@ -779,9 +779,9 @@ def extractSimData(
         print('no length measurements')
         return None
 
-    if phys_params.erosion_height_start.val < np.min(ht_sampled) + 2_000:
-        print('start height')
-        return None
+    # if phys_params.erosion_height_start.val < np.min(ht_sampled) + 2_000:
+    #     print('start height')
+    #     return None
 
     # If the simulation should only be checked that it's good, return the postprocess parameters used to
     #   generate the data
