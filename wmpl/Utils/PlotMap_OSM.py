df062891dcdaf5dd79eeaef47270ccf7536910d7
Milan Kalina <milank2010@gmail.com>
2022-07-10 19:07:16 +0200

    Improve OSM map quality

--- wmpl/Utils/PlotMap_OSM.py
+++ wmpl/Utils/PlotMap_OSM.py
@@ -160,7 +160,7 @@ class OSMMap(object):
         self.ax.set_extent(extent)
         
         # adding map with zoom ratio
-        self.ax.add_image(request, 6)
+        self.ax.add_image(request, 7, interpolation='spline36')
 
 
     def scatter(self, lat_list, lon_list, **kwargs):
