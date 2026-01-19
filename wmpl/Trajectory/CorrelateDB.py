12dab86bdf5505d6a9cdd63e54daf62729c54e70
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2026-01-19 17:45:20 +0000

    start adding some standalone management to the sqlite db scripts

--- wmpl/Trajectory/CorrelateDB.py
+++ wmpl/Trajectory/CorrelateDB.py
@@ -4,6 +4,8 @@ import os
 import sqlite3
 import logging
 import logging.handlers
+import argparse
+import datetime
 log = logging.getLogger("traj_correlator")
 
 
@@ -88,3 +90,63 @@ def openTrajDatabase(db_path, db_name='processed_trajectories'):
         cur.execute("CREATE TABLE trajectories()")
     con.commit()
     return con
+
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description="""Automatically compute trajectories from RMS data in the given directory.""",
+        formatter_class=argparse.RawTextHelpFormatter)
+
+    arg_parser.add_argument('--dir_path', type=str, default=None, help='Path to the directory containing the databases.')
+
+    arg_parser.add_argument('--database', type=str, default=None, help='Database to process, either observations or trajectories')
+
+    arg_parser.add_argument('--action', type=str, default=None, help='Action to take on the database')
+
+    arg_parser.add_argument("--logdir", type=str, default=None,
+        help="Path to the directory where the log files will be stored. If not given, a logs folder will be created in the database folder")
+      
+    arg_parser.add_argument('-r', '--timerange', metavar='TIME_RANGE',
+        help="""Apply action to this date range in the format: "(YYYYMMDD-HHMMSS,YYYYMMDD-HHMMSS)".""", type=str)
+    
+    cml_args = arg_parser.parse_args()
+    # Find the log directory
+    log_dir = cml_args.logdir
+    if log_dir is None:
+        log_dir = os.path.join(cml_args.dir_path, 'logs')
+    if not os.path.isdir(log_dir):
+        os.makedirs(log_dir)
+    log.setLevel(logging.DEBUG)
+
+    # Init the log formatter
+    log_formatter = logging.Formatter(
+        fmt='%(asctime)s-%(levelname)-5s-%(module)-15s:%(lineno)-5d- %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S')
+
+    # Init the file handler
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_file = os.path.join(log_dir, f"correlate_db_{timestamp}.log")
+    file_handler = logging.handlers.TimedRotatingFileHandler(log_file, when="midnight", backupCount=7)
+    file_handler.setFormatter(log_formatter)
+    log.addHandler(file_handler)
+
+    # Init the console handler (i.e. print to console)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    log.addHandler(console_handler)
+
+    dbname = cml_args.database.lower()
+    action = cml_args.action.lower()
+
+    if dbname == 'observations':
+        dbhandle = openObsDatabase(cml_args.dir_path)
+        if action == 'read':
+            cur = dbhandle.cursor()
+            cur.execute('select * from paired_obs where status=1')
+            print(f'there are {len(cur.fetchall())} paired obs')
+            cur.execute('select * from paired_obs where status=0')
+            print(f'and       {len(cur.fetchall())} unpaired obs')
+        closeObsDatabase(dbhandle)
+    elif dbname == 'trajectories':
+        print('hello')
+    else:
+        log.info('valid database not specified')
